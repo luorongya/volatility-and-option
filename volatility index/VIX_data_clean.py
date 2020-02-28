d10df5eb0d4d99cd19bb3887ef1e6645204b3384
Rongya Luo <luorongya@pku.edu.cn>
2020-02-28 23:09:45 +0800

    modified highest strike price selection under forward price, added an output to excel of VIX indices, and added SSE 300 etf options updates, w/ other minor revisions.

--- volatility index/VIX_data_clean.py
+++ volatility index/VIX_data_clean.py
@@ -6,27 +6,45 @@ Created on Thu Sep 27 14:45:19 2018
 """
 
 import pandas as pd
+import os
 
-basic = pd.read_excel('E:/data/50ETF期权合约基本资料.xlsx', parse_date=True)
+# import basic contract info
+# basic = pd.read_excel(os.getcwd()+'\\data\\50ETF期权合约基本资料.xlsx', parse_date=True)
+basic = pd.read_excel(os.getcwd()+'\\data\\沪300ETF期权合约基本资料.xlsx', parse_date=True)
 basic = basic.loc[:, ['trade_code', 'type', 'expire']]
-data2015 = pd.read_excel('E:/data/50ETF期权日行情2015-2016.xlsx', parse_date=True)
-data2017 = pd.read_excel('E:/data/50ETF期权日行情2017-2018.xlsx', parse_date=True)
-data2015 = data2015.loc[:, ['date', 'trade_code', 'strike', 'settle']]
-data2017 = data2017.loc[:, ['date', 'trade_code', 'strike', 'settle']]
-data = pd.concat([data2015, data2017])
-datas = pd.merge(data, basic, how='left') # 按trade_code合并
-datas.drop(columns='trade_code', inplace=True) # 删除trade_code列
-calls = datas[datas['type'] == 'call'] # 分离出认购期权的数据
-puts = datas[datas['type'] == 'put'] # 分离出认沽期权的数据
+
+# import daily quotes
+# data2015 = pd.read_excel(os.getcwd()+'\\data\\50ETF期权日行情2015-2016.xlsx', parse_date=True)
+# data2017 = pd.read_excel(os.getcwd()+'\\data\\50ETF期权日行情2017-2018.xlsx', parse_date=True)
+# data2018 = pd.read_excel(os.getcwd()+'\\data\\50ETF期权日行情2018补.xlsx', parse_date=True)
+# data2019 = pd.read_excel(os.getcwd()+'\\data\\50ETF期权日行情2019.xlsx', parse_date=True)
+# data2020 = pd.read_excel(os.getcwd()+'\\data\\50ETF期权日行情2020.xlsx', parse_date=True)
+# data2015 = data2015.loc[:, ['date', 'trade_code', 'strike', 'settle']]
+# data2017 = data2017.loc[:, ['date', 'trade_code', 'strike', 'settle']]
+# data2018 = data2018.loc[:, ['date', 'trade_code', 'strike', 'settle']]
+# data2020 = data2020.loc[:, ['date', 'trade_code', 'strike', 'settle']]
+# data = pd.concat([data2015, data2017, data2018, data2019, data2020])
+
+# import daily quotes(sse 300etf)
+data2020 = pd.read_excel(os.getcwd()+'\\data\\沪300ETF期权日行情2019-2020.xlsx', parse_date=True)
+data2020 = data2020.loc[:, ['date', 'trade_code', 'strike', 'settle']]
+data = data2020
+
+# separate call and put options
+datas = pd.merge(data, basic, how='left')  # 按trade_code合并
+datas.drop(columns='trade_code', inplace=True)  # 删除trade_code列
+calls = datas[datas['type'] == '认购']  # 分离出认购期权的数据
+puts = datas[datas['type'] == '认沽']  # 分离出认沽期权的数据
 calls.rename(columns={'settle': 'call'}, inplace=True)
 puts.rename(columns={'settle': 'put'}, inplace=True)
+
 # 按交易日期、到期日、执行价为连接键合并，并删除重复数据
 options = pd.merge(calls, puts.loc[:, ['date', 'expire', 'strike', 'put']], 
                    on=['date', 'expire', 'strike'], how='left').drop_duplicates()
 # 计算剩余到期的天数（自然日）
 options = options.loc[:, ['date', 'expire', 'strike', 'call', 'put']]
-
 options['T_days'] = (options['expire'] - options['date']).apply(lambda x: x.days)
+
 
 def small(x, s):
     '''确定第（s+1）小的值'''
@@ -34,17 +52,22 @@ def small(x, s):
     ts.sort()
     return ts[s]
 
-last1 = options.groupby('date')['T_days'].apply(small, s=0) # 交易日第一短到期天数
-last2 = options.groupby('date')['T_days'].apply(small, s=1) # 交易日第二短到期天数
-last3 = options.groupby('date')['T_days'].apply(small, s=2) # 交易日第三短到期天数
+
+last1 = options.groupby('date')['T_days'].apply(small, s=0)  # 交易日第一短到期天数
+last2 = options.groupby('date')['T_days'].apply(small, s=1)  # 交易日第二短到期天数
+last3 = options.groupby('date')['T_days'].apply(small, s=2)  # 交易日第三短到期天数
 last = pd.concat([last1, last2, last3], axis=1)
 last.columns = ['last1', 'last2', 'last3']
 for i in range(len(last)):
     if last['last1'][i] <= 7: # 近月合约到期期限必须不少于1个星期
-        last.loc[last.index[i], 'last1'] = last.loc[last.index[i],'last2']
-        last.loc[last.index[i], 'last2'] = last.loc[last.index[i],'last3']
+        last.loc[last.index[i], 'last1'] = last.loc[last.index[i], 'last2']
+        last.loc[last.index[i], 'last2'] = last.loc[last.index[i], 'last3']
 options = pd.merge(options, last, on='date', how='left')
 
-options.to_excel('E:/data/50ETF_option_VIX.xlsx')
-with pd.HDFStore('E:/data/market.h5') as store:
-    store['50ETF_option_VIX'] = options
+# options.to_excel(os.getcwd()+'\\data\\50ETF_option_VIX_2020.xlsx')
+options.to_excel(os.getcwd()+'\\data\\沪300ETF_option_VIX_2020.xlsx')
+data_file = os.getcwd()+'\\data\\market_300_2020.h5'
+shibor = pd.read_excel(os.getcwd()+'\\data\\shibor_3M.xlsx', index_col='date')
+with pd.HDFStore(data_file) as store:
+    store['SSE300ETF_option_VIX'] = options
+    store['shibor_3M'] = shibor
--- volatility index/VIX_new.py
+++ volatility index/VIX_new.py
@@ -3,14 +3,17 @@
 Created on Thu Sep 27 20:33:38 2018
 
 @author: 54326
+
+forked from Jensenberg/volatility-and-option on Feb 27 2020
 """
 
 from math import exp, sqrt
 
+
 class Vix():
     '''
     定义一个用方差互换和波动率互换方法编制VIX指数的类，基于50ETF期权
-    '''    
+    '''
     def __init__(self, T_days, strikes, calls, puts, r):
         '''
         T_days:
@@ -29,7 +32,7 @@ class Vix():
         self.calls = calls
         self.puts = puts
         self.r = r
-        
+
     def T(self, Y_days=365):
         '''
         计算剩余期限按年计算的比例
@@ -37,23 +40,27 @@ class Vix():
             一年的总天数
         '''
         return self.T_days / Y_days
-    
+
     def F(self):
         '''
         看涨期权和看跌期权价差绝对值最小对应的执行价格的远期价格
         '''
         C_P = abs(self.calls - self.puts)
         K_F = C_P.idxmin()
+        # print('认购认沽价差list：\n',  C_P)
+        # print('最小认购认沽价差：\n', K_F)
+        # print('远期价格：\n', K_F + (self.calls[K_F] - self.puts[K_F]) * exp(self.r * self.T()))
         return K_F + (self.calls[K_F] - self.puts[K_F]) * exp(self.r * self.T())
-    
+
     def Ks(self):
         '''
         将执行价格从小到大排序，并返回列表
         '''
         ks = list(self.strikes)
         ks.sort()
+        # print('执行价格从小到大排序：\n', ks)
         return ks
-    
+
     def K0(self):
         '''
         低于F值的第一个执行价格为K0
@@ -61,10 +68,15 @@ class Vix():
         ks = self.Ks()
         K_F = self.F()
         i = 0
-        while ks[i] < K_F:
-            i += 1
-        return ks[i-1]
-    
+        if ks[-1] < K_F:
+            k0 = ks[-1]
+        else:
+            while ks[i] < K_F and i < len(ks):
+                i += 1
+            k0 = ks[i-1]
+        # print('低于远期的第一个执行价格：\n', k0)
+        return k0
+
     def delta_K(self):
         '''
         执行价格的价差为相邻价格差的均值，[K(i+1) - K(i-1)] / 2
@@ -76,8 +88,9 @@ class Vix():
         delta_k[ks[-1]] = ks[-1] - ks[-2]
         for i in range(1, len(ks) - 1):
             delta_k[ks[i]] = (ks[i+1] - ks[i-1]) / 2
+        # print('执行价格价差 delta_k:\n', delta_k)
         return delta_k
-    
+
     def Q_K(self):
         '''
         期权费用，按结算价计算
@@ -93,6 +106,7 @@ class Vix():
                 q_k[k] = self.calls[k]
             else:
                 q_k[k] = (self.calls[k] + self.puts[k]) / 2
+        # print('期权价格：\n', q_k)
         return q_k
 
     def sigma2(self):
@@ -106,7 +120,7 @@ class Vix():
         sum_K = sum(delta_k[k] / k**2 * q_k[k] * exp(self.r * T)
                     for k in ks)
         return 2*sum_K / T - (self.F()/self.K0() - 1) ** 2 / T
-    
+
     def volatility(self, other, M_days=30, Y_days=365):
         '''
         将近月和次近月期权的波动率加权，得到对未来30天的波动率的预期值
@@ -119,18 +133,28 @@ class Vix():
                 / (other.T_days - self.T_days)
         last2 = other.sigma2() * other.T() * (M_days - self.T_days)\
                 / (other.T_days - self.T_days)
-        return 100 * sqrt((last1 + last2)  * Y_days / M_days)
+        # if self.T_days < 30:
+        #     vix_num = 100 * sqrt((last1 + last2) * Y_days / M_days)
+        # else:
+        #     vix_num = 100 * sqrt(last1)
+        vix_num = 100 * sqrt((last1 + last2) * Y_days / M_days)
+        # print('近月合约剩余天数：', self.T_days, '\tVIX 指数：', vix_num)
+        return vix_num
+
+
 
 if __name__ == '__main__':
     import pandas as pd
     import matplotlib.pyplot as plt
-    
-    with pd.HDFStore('E:/data/market.h5') as store: 
-        data = store['50ETF_option_VIX']
+
+    data_file = 'C:\\Users\\victo\\Projects\\volatility\\data\\market_300_2020.h5'
+    with pd.HDFStore(data_file) as store:
+        # data = store['50ETF_option_VIX']
+        data = store['SSE300ETF_option_VIX']
         shibor = store['shibor_3M']
+    dates = data['date'].drop_duplicates()
     data.set_index(['date', 'expire'], drop=False, inplace=True)
-    dates = data['date'].drop_duplicates() 
-    
+
     def data_slice(data, last_flag):
         '''
         提取近月或次近月的期权数据
@@ -138,46 +162,49 @@ if __name__ == '__main__':
             data:
                 某一个交易的所有期权的数据
             last_flag:
-                近月或次近月的标志，为’last1'或'last2'
+                近月或次近月的标志，为'last1'或'last2'
         Returns:
             执行价格序列，看涨期权价格序列，看跌期权价格序列
         '''
         data_last = data[data['T_days'] == data[last_flag]]
         data_last.set_index('strike', drop=False, inplace=True)
         return data_last['strike'], data_last['call'], data_last['put']
-    
+
     def vix(T_days, data, last_flag, r):
         '''
         返回一个Vix实例
         '''
-        strikes, calls, puts = data_slice(data, last_flag) 
+        strikes, calls, puts = data_slice(data, last_flag)
         return Vix(T_days, strikes, calls, puts, r)
-    
-    VIX = {} #将得到的volatility值，以日期为键，保存到字典VIX
+
+    VIX = {}  # 将得到的volatility值，以日期为键，保存到字典VIX
     for date in dates:
+        print(date)
         r = shibor.loc[date, 'shibor_3M']
-        data_t = data.xs(date, level=0) #某一交易日的数据
-        T_days_1 = data_t['last1'][0] #近月的剩余到期天数
-        T_days_2 = data_t['last2'][0] #次近月的剩余到期天数
-        vix_1 =  vix(T_days_1, data_t, 'last1', r)
-        vix_2 =  vix(T_days_2, data_t, 'last2', r)
-        VIX[date] = vix_1.volatility(vix_2) 
-    
+        data_t = data.xs(date, level=0)  # 某一交易日的数据
+        T_days_1 = data_t['last1'][0]  # 近月的剩余到期天数
+        T_days_2 = data_t['last2'][0]  # 次近月的剩余到期天数
+        vix_1 = vix(T_days_1, data_t, 'last1', r)
+        vix_2 = vix(T_days_2, data_t, 'last2', r)
+        VIX[date] = vix_1.volatility(vix_2)
+
     VIX = pd.Series(VIX).sort_index()
     plt.figure(figsize=(15, 8))
     plt.plot(VIX)
     plt.xticks(fontsize=14)
     plt.yticks(fontsize=14)
     plt.xlabel('date', fontsize=16)
-    max_index = VIX.idxmax().strftime('%Y/%m/%d')
-    max_value = VIX.max()
-    plt.annotate('%s VIX=%.2f, ' %(max_index, max_value) 
-                +'rise margin ratio from 12% to 20%', 
-                 xy=(max_index, VIX[max_index]),
-                 xytext=(50, 0),
-                 textcoords='offset points',
-                 arrowprops=dict(arrowstyle="->"),
-                 fontsize=16)
-    plt.ylabel('VIX', fontsize=16)
-    plt.title('VIX-Demeterfi', fontsize=16)
-    plt.savefig('VIX_new.png',bbox_inches='tight')    
+    # max_index = VIX.idxmax().strftime('%Y/%m/%d')
+    # max_value = VIX.max()
+    # plt.annotate('%s VIX=%.2f, ' %(max_index, max_value)
+    #             +'rise margin ratio from 12% to 20%',
+    #              xy=(max_index, VIX[max_index]),
+    #              xytext=(50, 0),
+    #              textcoords='offset points',
+    #              arrowprops=dict(arrowstyle="->"),
+    #              fontsize=16)
+    plt.ylabel('iVX', fontsize=16)
+    plt.title('iVX', fontsize=16)
+    plt.savefig('iVX_300.png', bbox_inches='tight')
+
+    VIX.to_excel('C:\\Users\\victo\\Projects\\volatility\\data\\沪300ETF_option_VIX_2020_stats.xlsx')
